0e6988d310bb946c2ef3cb2eacd1733c0af0ebf3
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-06-18 13:30:37 +0600

    add Feature Properties section

---
+++ src/Sections/FeaturedProperties.jsx
@@ -0,0 +1,27 @@
+import PropertyCard from '@/components/PropertyCard';
+import React from 'react';
+
+const FeaturedProperties = async () => {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/featured-properties`, { cache: "no-store" });
+    const fProperties = await res.json();
+
+    return (
+        <div>
+            <div className="text-center mt-5 mb-12">
+                <h2 className="text-4xl text-[#0D0D33] md:text-5xl font-bold mb-4">
+                    Featured Properties
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-5 justify-center gap-10">
+                    {
+                        fProperties.map((property, ind) =>
+                            <PropertyCard key={ind} property={property} />)
+
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedProperties;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/Sections/Banner";
+import FeaturedProperties from "@/Sections/FeaturedProperties";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <FeaturedProperties />
 
     </div>
   );
