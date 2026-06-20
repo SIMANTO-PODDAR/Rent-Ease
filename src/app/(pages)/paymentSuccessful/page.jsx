85431bfc3e2d86067eb65a86f53ebe5895ddf182
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-06-20 12:53:52 +0600

    Update Booking Data

--- src/app/(pages)/paymentSuccessful/page.jsx
+++ src/app/(pages)/paymentSuccessful/page.jsx
@@ -1,21 +1,51 @@
 import { stripe } from "@/lib/stripe";
+import { redirect } from 'next/navigation';
 
 const PaymentSuccessfulPage = async ({ searchParams }) => {
 
     const { session_id } = await searchParams;
     if (!session_id) {
-        throw new Error
+        return redirect('/')
     }
 
-    console.log("Session ID:", session_id);
+    // console.log("Session ID:", session_id);
 
     const session = await stripe.checkout.sessions.retrieve(session_id);
+    const bookingId = session.metadata.bookingId;
+    const transactionId = session.payment_intent;
 
-    return (
-        <div>
-            Payment Successful
-        </div>
-    );
+    const Data = {
+        // Update Booking Data
+        transactionId: transactionId,
+        paymentStatus: 'Paid',
+    };
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/all-bookings/${bookingId}`,
+        {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+            },
+
+            body: JSON.stringify(Data)
+        });
+
+    if (res.ok == true) {
+        return (
+            <div>
+                Payment Successful
+                Your TransactionId: {transactionId}
+            </div>
+        );
+    }
+
+    else {
+        return (
+            <div>
+                !
+            </div>
+        );
+    };
 };
 
 export default PaymentSuccessfulPage;
